07536aa2c0538321e386544dc3de3827f97b2d44
anmink <anne-maina@web.de>
2026-06-04 23:20:45 +0200

    add routes to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
-<script setup lang="ts">
-import HomeView from './views/HomeView.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <HomeView />
+  <nav class="bg-white border-b px-4 py-3 flex gap-4">
+    <RouterLink to="/" class="font-semibold text-blue-600">Spiele</RouterLink>
+    <RouterLink to="/bets" class="font-semibold text-blue-600">Meine Wetten</RouterLink>
+  </nav>
+  <RouterView />
 </template>
-
-<style scoped></style>
