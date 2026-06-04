a274209c2dbafffc14d82564d7dfc36ba584e7c6
anmink <anne-maina@web.de>
2026-06-04 23:18:16 +0200

    add bet route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: HomeView },
+    {
+      path: '/bets',
+      component: () => import('@/views/BetsView.vue'),
+    },
+  ],
 })
 
 export default router
